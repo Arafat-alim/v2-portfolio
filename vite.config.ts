ce3b3a56daf6bfaa32511f7305096109ed54d4d2
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-05 02:10:36 +0530

    ☑️ Server issue has been fixed.

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,18 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
+  publicDir: "public",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
