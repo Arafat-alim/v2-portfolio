8aae885c1ea484e706838ce330859df08c535895
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-08 00:59:28 +0530

    ‼️ `Fixed`: Backend is not live.

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://v2-backend-tawny.vercel.app",
         changeOrigin: true,
       },
     },
